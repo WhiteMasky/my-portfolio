5028e275c554eb154e601753fb920bcd9dc707eb
Yichi Zhang <u7748799@anu.edu.au>
2024-04-11 16:53:32 +1000

    head image change

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -13,8 +13,6 @@ import photo11 from "../assets/img/photo11.png";
 import photo12 from "../assets/img/photo12.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Gallery = () => {
