290c1aa0128a5da90d7a187cff782b50468dec7e
Yichi Zhang <u7748799@anu.edu.au>
2024-04-16 17:36:20 +1000

    main CTA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
@@ -16,17 +16,44 @@ import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
 function App() {
+
+  const [showPage, setShowPage] = useState(false);
+
+  const handleClick = () => {
+    setShowPage(true);
+  };
+
   return (
-    <div className="App">
-        <NavBar />
-        <Banner />
-        <About />
-        <Gallery />
-        <Projects />
-        <Contact />
-        <Footer />
+    <div>
+      <h1>Hi! <br />
+      Welcome to my world.</h1>
+      {!showPage && (
+        <button onClick={handleClick}>Click here to know me more</button>
+      )}
+      {showPage &&
+        <div className="App">
+          <NavBar />
+          <Banner />
+          <About />
+          <Gallery />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>}
     </div>
   );
+
+  // return (
+  //   <div className="App">
+  //     <NavBar />
+  //     <Banner />
+  //     <About />
+  //     <Gallery />
+  //     <Projects />
+  //     <Contact />
+  //     <Footer />
+  //   </div>
+  // );
 }
 
 export default App;
